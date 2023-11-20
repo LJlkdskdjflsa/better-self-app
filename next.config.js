0db86eed0242cff175e9596bb5317dcbe035356f
LJlkdskdjflsa <liliangjya@gmail.com>
2023-11-20 23:02:31 +0800

    feat: update PWA configuration and manifest.json

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable:
-    process.env.NODE_ENV === "development" ||
-    process.env.NODE_ENV === "preview" ||
-    process.env.NODE_ENV === "production",
+  disable: process.env.NODE_ENV !== 'production',
   // delete two lines above to enable PWA in production deployment
   // add your own icons to public/manifest.json
   // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
@@ -14,6 +11,6 @@ module.exports = withPWA({
   swcMinify: true,
   reactStrictMode: true,
   eslint: {
-    dirs: ["src"],
+    dirs: ['src'],
   },
 });

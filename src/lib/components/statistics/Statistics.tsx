5c3f40ecff816b36b339a06157c23759dc873da5
LJlkdskdjflsa <liliangjya@gmail.com>
2023-11-24 22:37:24 +0800

    feat: add switch to choose date button

--- src/lib/components/statistics/Statistics.tsx
+++ src/lib/components/statistics/Statistics.tsx
@@ -1,4 +1,11 @@
-import { useColorMode } from '@chakra-ui/react';
+import {
+  Button,
+  Text,
+  Flex,
+  Spacer,
+  useColorMode,
+  Box,
+} from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import {
   PieChart,
@@ -26,6 +33,12 @@ export default function Statistics() {
   const [selectedDate, setSelectedDate] = useState(
     new Date().toISOString().split('T')[0]
   );
+
+  const changeDate = (delta: number) => {
+    const currentDate = new Date(selectedDate);
+    currentDate.setDate(currentDate.getDate() + delta);
+    setSelectedDate(currentDate.toISOString().split('T')[0]);
+  };
 
   useEffect(() => {
     const loadRecords = async () => {
@@ -61,55 +74,89 @@ export default function Statistics() {
   return (
     <div>
       <h2>Statistics</h2>
-      <input
-        type="date"
-        value={selectedDate}
-        onChange={(e) => setSelectedDate(e.target.value)}
-      />
-      <PieChart width={400} height={400}>
-        <Pie
-          data={pieChartDataArray}
-          cx={200}
-          cy={200}
-          outerRadius={80}
-          fill="#8884d8"
-          dataKey="value"
-          label={(entry) => entry.name}
-        >
-          {pieChartDataArray.map((entry) => (
-            <Cell
-              key={entry.id}
-              fill={COLORS[pieChartDataArray.indexOf(entry) % COLORS.length]}
-            />
-          ))}
-        </Pie>
-        <Tooltip content={<PieChartTooltip colorMode={colorMode} />} />
-      </PieChart>
-      <LineChart
-        width={500}
-        height={300}
-        data={pieChartDataArray}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="created_at" />
-        <YAxis yAxisId="left" />
-        <YAxis yAxisId="right" orientation="right" />
-        <Tooltip content={<LineChartTooltip colorMode={colorMode} />} />
-        <Legend />
-        <Line yAxisId="left" type="monotone" dataKey="focus" stroke="#8884d8" />
-        <Line
-          yAxisId="right"
-          type="monotone"
-          dataKey="point"
-          stroke="#82ca9d"
+      <Box pt={5}>
+        <input
+          type="date"
+          value={selectedDate}
+          onChange={(e) => setSelectedDate(e.target.value)}
         />
-      </LineChart>
+      </Box>
+
+      {records.length > 0 ? (
+        <>
+          <PieChart width={400} height={400}>
+            <Pie
+              data={pieChartDataArray}
+              cx={200}
+              cy={200}
+              outerRadius={80}
+              fill="#8884d8"
+              dataKey="value"
+              label={(entry) => entry.name}
+            >
+              {pieChartDataArray.map((entry) => (
+                <Cell
+                  key={entry.id}
+                  fill={
+                    COLORS[pieChartDataArray.indexOf(entry) % COLORS.length]
+                  }
+                />
+              ))}
+            </Pie>
+            <Tooltip content={<PieChartTooltip colorMode={colorMode} />} />
+          </PieChart>
+          <LineChart
+            width={500}
+            height={300}
+            data={pieChartDataArray}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="created_at" />
+            <YAxis yAxisId="left" />
+            <YAxis yAxisId="right" orientation="right" />
+            <Tooltip content={<LineChartTooltip colorMode={colorMode} />} />
+            <Legend />
+            <Line
+              yAxisId="left"
+              type="monotone"
+              dataKey="focus"
+              stroke="#8884d8"
+            />
+            <Line
+              yAxisId="right"
+              type="monotone"
+              dataKey="point"
+              stroke="#82ca9d"
+            />
+          </LineChart>
+        </>
+      ) : (
+        // Render a friendly message if there are no records
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          height="300px"
+          borderWidth="1px"
+          borderRadius="lg"
+          p={4}
+          borderColor="gray.200"
+        >
+          <Text fontSize="xl" color="gray.500">
+            No data available for this day.
+          </Text>
+        </Flex>
+      )}
+      <Flex p={5}>
+        <Button onClick={() => changeDate(-1)}>Previous Day</Button>
+        <Spacer />
+        <Button onClick={() => changeDate(1)}>Next Day</Button>
+      </Flex>
     </div>
   );
 }

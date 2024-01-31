37e4b6ca8ec43613f6ce3d9ff79ef4bae0dec3c3
LJlkdskdjflsa <liliangjya@gmail.com>
2024-01-31 23:58:52 +0800

    feat: applicant card modal

--- src/lib/components/dashboard/ApplicantCard.tsx
+++ src/lib/components/dashboard/ApplicantCard.tsx
@@ -1,7 +1,24 @@
 'use client';
 
 import { DeleteIcon } from '@chakra-ui/icons';
-import { Box, IconButton, ScaleFade, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  IconButton,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  ScaleFade,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
 import _ from 'lodash';
 import { memo } from 'react';
 
@@ -15,6 +32,61 @@ type ApplicantCardProps = {
   onDelete: (id: ApplicantModelNew['id']) => void;
   onDropHover: (i: number, j: number) => void;
 };
+
+function ApplicantModal({
+  task,
+  isOpen,
+  onClose,
+}: {
+  task: ApplicantModelNew;
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>
+          <Text
+            fontWeight="semibold"
+            cursor="inherit"
+            border="none"
+            p={0}
+            resize="none"
+            color="gray.700"
+          >
+            {task.first_name}
+          </Text>
+          <Text color="gray.400" fontSize="sm">
+            {task.position.job}
+          </Text>
+        </ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          {/* <FormControl>
+            <FormLabel>公司</FormLabel>
+            <Input defaultValue={task.first_name} />
+          </FormControl> */}
+
+          <FormControl mt={4}>
+            <FormLabel>職位</FormLabel>
+            <Input defaultValue={task.position.job} />
+          </FormControl>
+
+          <FormControl mt={4}>
+            <FormLabel>應徵日期</FormLabel>
+            <Input defaultValue={task.apply_date} />
+          </FormControl>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
 
 function ApplicantCard({
   index,
@@ -32,60 +104,66 @@ function ApplicantCard({
     handleDelete(task.id);
   };
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <ScaleFade in unmountOnExit>
-      <Box
-        ref={ref}
-        as="div"
-        role="group"
-        position="relative"
-        rounded="lg"
-        pl={3}
-        pr={7}
-        pt={3}
-        pb={1}
-        boxShadow="xl"
-        cursor="grab"
-        fontWeight="bold"
-        userSelect="none"
-        // bgColor={task.color}
-        bgColor="white"
-        opacity={isDragging ? 0.5 : 1}
-      >
-        <IconButton
-          position="absolute"
-          top={0}
-          right={0}
-          zIndex={100}
-          aria-label="delete-task"
-          size="md"
-          colorScheme="solid"
-          color="gray.700"
-          icon={<DeleteIcon />}
-          opacity={0}
-          _groupHover={{
-            opacity: 1,
-          }}
-          onClick={handleDeleteClick}
-        />
-        <Text
-          fontWeight="semibold"
-          cursor="inherit"
-          border="none"
-          p={0}
-          resize="none"
-          // minH={70}
-          // maxH={200}
-          color="gray.700"
-          // onChange={handleTitleChange}
+    <>
+      <ScaleFade in unmountOnExit>
+        <Box
+          ref={ref}
+          as="div"
+          role="group"
+          position="relative"
+          rounded="lg"
+          pl={3}
+          pr={7}
+          pt={3}
+          pb={1}
+          boxShadow="xl"
+          cursor="grab"
+          fontWeight="bold"
+          userSelect="none"
+          // bgColor={task.color}
+          bgColor="white"
+          opacity={isDragging ? 0.5 : 1}
+          onClick={onOpen}
         >
-          {task.first_name}
-        </Text>
-        <Text color="gray.400" fontSize="sm">
-          {task.position.job}
-        </Text>
-      </Box>
-    </ScaleFade>
+          <IconButton
+            position="absolute"
+            top={0}
+            right={0}
+            zIndex={100}
+            aria-label="delete-task"
+            size="md"
+            colorScheme="solid"
+            color="gray.700"
+            icon={<DeleteIcon />}
+            opacity={0}
+            _groupHover={{
+              opacity: 1,
+            }}
+            onClick={handleDeleteClick}
+          />
+          <Text
+            fontWeight="semibold"
+            cursor="inherit"
+            border="none"
+            p={0}
+            resize="none"
+            // minH={70}
+            // maxH={200}
+            color="gray.700"
+            // onChange={handleTitleChange}
+          >
+            {task.first_name}
+          </Text>
+          <Text color="gray.400" fontSize="sm">
+            {task.position.job}
+          </Text>
+        </Box>
+      </ScaleFade>
+      <ApplicantModal task={task} isOpen={isOpen} onClose={onClose} />
+    </>
   );
 }
 

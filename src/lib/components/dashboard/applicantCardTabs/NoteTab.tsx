ed71ad5a713f3a55a8244e84e1faa0ceb918c30c
LJlkdskdjflsa <liliangjya@gmail.com>
2024-02-01 15:26:27 +0800

    feat: delete note function

--- src/lib/components/dashboard/applicantCardTabs/NoteTab.tsx
+++ src/lib/components/dashboard/applicantCardTabs/NoteTab.tsx
@@ -4,11 +4,15 @@ import {
   Flex,
   FormControl,
   FormLabel,
+  Icon,
+  IconButton,
   Input,
+  Spacer,
   TabPanel,
   Text,
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
+import { FaTrash } from 'react-icons/fa';
 
 import { useNotes } from '../hooks/useNotes';
 import type { ApplicantModelNew } from '../model';
@@ -18,7 +22,7 @@ interface NoteTabProps {
 }
 
 const NoteTab: React.FC<NoteTabProps> = ({ task }) => {
-  const { notes, fetchNotes } = useNotes(task.id);
+  const { notes, fetchNotes, deleteNote } = useNotes(task.id);
   const [newNote, setNewNote] = useState('');
 
   const addNote = () => {
@@ -51,21 +55,37 @@ const NoteTab: React.FC<NoteTabProps> = ({ task }) => {
   return (
     <TabPanel>
       {notes.map((note) => (
-        <Box w="100%">
+        <Box w="100%" p={2} m={2} border="1px solid gray" borderRadius="md">
           <FormControl key={note.id}>
             <Flex>
               <Text>{note.description}</Text>
-              {/* <Text>{note.created_date}</Text> */}
+              <Spacer />
+              <IconButton
+                icon={<Icon as={FaTrash} />}
+                colorScheme="red"
+                onClick={() => deleteNote(note.id)}
+                aria-label=""
+              />
             </Flex>
           </FormControl>
         </Box>
       ))}
 
-      <FormControl>
-        <FormLabel>新增備註</FormLabel>
-        <Input value={newNote} onChange={(e) => setNewNote(e.target.value)} />
-        <Button onClick={addNote}>新增</Button>
-      </FormControl>
+      <Box m={2}>
+        <FormControl>
+          <FormLabel>新增備註</FormLabel>
+          <Flex w="100%">
+            <Input
+              value={newNote}
+              onChange={(e) => setNewNote(e.target.value)}
+            />
+            <Spacer />
+            <Button ml={10} onClick={addNote}>
+              新增
+            </Button>
+          </Flex>
+        </FormControl>
+      </Box>
     </TabPanel>
   );
 };
--- src/lib/components/dashboard/hooks/useNotes.ts
+++ src/lib/components/dashboard/hooks/useNotes.ts
@@ -17,9 +17,29 @@ export const useNotes = (taskID: number) => {
       });
   }, [taskID]);
 
+  const deleteNote = useCallback(
+    (noteID: number) => {
+      fetch(`http://127.0.0.1:8001/api/positionapps/${taskID}/notes/`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer AFG9JxtaRz79cjLZnhuz406uypiae6',
+        },
+        body: JSON.stringify({ jobapp_note_id: noteID }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            fetchNotes(); // Fetch all notes again
+          }
+        });
+    },
+    [taskID, fetchNotes]
+  );
+
   useEffect(() => {
     fetchNotes();
   }, [fetchNotes]);
 
-  return { notes, fetchNotes };
+  return { notes, fetchNotes, deleteNote };
 };
